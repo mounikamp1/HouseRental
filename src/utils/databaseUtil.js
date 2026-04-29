7d8d8008b4e3e0967fc96da312901fae3a8c783a
Mounika <mounika.mandarapu12@gmail.com>
2026-04-29 18:20:39 -0500

    security: remove hardcoded Atlas URI from databaseUtil.js

--- src/utils/databaseUtil.js
+++ src/utils/databaseUtil.js
@@ -1,37 +1,3 @@
-// // const mysql = require("mysql2");
+﻿// Legacy MongoDB native driver helper - replaced by Mongoose (src/config/database.js)
+// This file is kept for reference only
 
-// // const pool = mysql.createPool({
-// //   host: "localhost",
-// //   user: "root",
-// //   password: "root",
-// //   database: "airbnb",
-// // });
-// // module.exports = pool.promise();
-// const mongo = require("mongodb");
-
-// const MongoClient = mongo.MongoClient;
-// const MONGO_URL =
-//   "***REMOVED***";
-
-// let _db;
-
-// const mongoConnect = (callback) => {
-//   MongoClient.connect(MONGO_URL)
-//     .then((client) => {
-//       callback();
-//       _db = client.db("airbnb");
-//     })
-//     .catch((err) => {
-//       console.log("Error While Connecting to Mongo: ", err);
-//     });
-// };
-
-// const getDB = () => {
-//   if (!_db) {
-//     throw new Error("Mongo not connected");
-//   }
-//   return _db;
-// };
-
-// exports.mongoConnect = mongoConnect;
-// exports.getDB = getDB;
